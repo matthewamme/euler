64e87c72f703e4fbdde19aeabe6af2eac23b00ab
matthewamme <matthew.amme@utexas.edu>
2014-04-20 23:41:51 -0500

    16py

---
+++ python/016/problem016.py
@@ -0,0 +1,14 @@
+"""
+Project Euler Problem #016
+
+What is the sum of the digits of the number 21000?
+"""
+
+import math,sys
+
+g = 20 #20x20 grid
+
+sum = 0
+for a in str(int(math.pow(2,1000))):
+   sum += int(a)
+print sum
